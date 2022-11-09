e6e5310e7ac8ef9adc75693ac3cfc23fcd95b14c
S556370 <S556370@nwmissouri.edu>
2022-11-09 13:17:19 -0600

    Misc: updated file name

--- routes/university.js
+++ routes/university.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('University', { title: 'Search Results for University Class' });
+  res.render('university', { title: 'Search Results for University Class' });
 });
 
 module.exports = router;
