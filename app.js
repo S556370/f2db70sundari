587219cef974f76ad0aa8ad659e37a69efd90865
S556370 <S556370@nwmissouri.edu>
2022-11-17 19:21:20 -0600

    Added some basic passport configurations in app.js and installed associated packages.

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ var mongodb = require('mongodb')
 
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+// allow access to passport
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+//end  of allow access to passport
+
 var universities = require("./models/university");
-
-
 require('dotenv').config();
 const connectionString =  process.env.MONGO_CON
 mongoose = require('mongoose');
@@ -44,6 +48,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//It will create session cookies that will be used in authentication and direct the app to use passport for the authentication.
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//end of authentication.
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -52,6 +67,16 @@ app.use('/university', universityRouter);
 app.use('/gridbuild', gridBuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+
+//to make the connection between passport and the database where the account information is going to be stored
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -76,11 +101,11 @@ async function recreateDB() {
   // Delete everything	
   await universities.deleteMany();
   let instance1 = new
-    universities({ "University_Id":1,"University_Name": "Northwest Missouri State University", "Capacity": 12000, "Location": "Maryville", "State": "Missouri", "Country": "USA" });
+    universities({ "University_Id": 1, "University_Name": "Northwest Missouri State University", "Capacity": 12000, "Location": "Maryville", "State": "Missouri", "Country": "USA" });
   let instance2 = new
-    universities({ "University_Id":2,"University_Name": "California State University", "Capacity": 480000, "Location": " Los Angeles", "State": "California", "Country": "USA" });
+    universities({ "University_Id": 2, "University_Name": "California State University", "Capacity": 480000, "Location": " Los Angeles", "State": "California", "Country": "USA" });
   let instance3 = new
-    universities({ "University_Id":3,"University_Name": "Alabama State University", "Capacity": 75000, "Location": "Montgomery", "State": "Alabama", "Country": "USA" });
+    universities({ "University_Id": 3, "University_Name": "Alabama State University", "Capacity": 75000, "Location": "Montgomery", "State": "Alabama", "Country": "USA" });
 
   instance1.save(function (err, doc) {
     if (err) return console.error(err);
@@ -97,5 +122,19 @@ async function recreateDB() {
 }
 let reseed = true;
 if (reseed) { recreateDB(); }
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }))
 
 module.exports = app;
