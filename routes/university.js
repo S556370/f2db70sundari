b9c0ec536e2ceafdb5580d3ca5ca838b2f2a0836
S556370 <S556370@nwmissouri.edu>
2022-11-29 18:52:54 -0600

    Added UI Components for CRUD

--- routes/university.js
+++ routes/university.js
@@ -3,11 +3,6 @@ var router = express.Router();
 
 var university_controller = require('../controllers/universities');
 /* GET home page. */
-
-router.get('/', university_controller.university_view_all_Page);
-router.get('/detail', university_controller.university_view_one_Page);
-router.get('/create', university_controller.university_create_Page);
-// router.get('/update', university_controller.university_update_Page);
 // redirect to login.
 const secured = (req, res, next) => {
     if (req.user) {
@@ -16,9 +11,17 @@ const secured = (req, res, next) => {
     req.session.returnTo = req.originalUrl;
     res.redirect("/login");
 }
-/* GET update costume page */
-router.get('/update', secured, university_controller.university_update_Page);
-router.get('/delete', university_controller.university_delete_Page);
 
+// router.get('/', university_controller.university_view_all_Page);
+// router.get('/detail', university_controller.university_view_one_Page);
+// router.get('/create', university_controller.university_create_Page);
+// router.get('/update', university_controller.university_update_Page);
+// router.get('/delete', university_controller.university_delete_Page);
+
+router.get('/', secured , university_controller.university_view_all_Page);
+router.get('/detail', secured, university_controller.university_view_one_Page);
+router.get('/create', secured, university_controller.university_create_Page);
+router.get('/update', secured, university_controller.university_update_Page);
+router.get('/delete', secured, university_controller.university_delete_Page);
 
 module.exports = router;
